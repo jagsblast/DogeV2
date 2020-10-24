0024164f53bb7b98791ccc8fad6f9a862f7cca50
jagsblast <33574320+jagsblast@users.noreply.github.com>
2020-10-24 22:16:18 +0100

    nav bar created for main page, changed if else to switch on php page

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
-$string = file_get_contents("https://raw.githubusercontent.com/jagsblast/Doge2/main/mock.json");
+$string = file_get_contents("https://raw.githubusercontent.com/jagsblast/DogeV2/master/mock.json");
 // echo $string;
 $json_a = json_decode($string, true);
 
 foreach ($json_a["servers"] as $key => $value) {
     echo $key;
     echo "<br/>";
-    // var_dump($value["status"]);
-    if ($value["status"] == "ok" || $value["status"] == "OK")
-    {
-        echo 'Status: ' . $value["status"] . "<br/> <br/>";
-    }
-    else
-    {
-        echo 'Status: ' . $value["status"] . "<br/>";
-        echo 'Message: ' . $value["message"] . "<br/>";
-        echo 'Raised: ' . $value["raised"] . "<br/>";
-        echo 'Updated: ' . $value["updated"] . "<br/>";
-        echo "<br/>";
-    }
+    $var = strtolower($value["status"]);
+
+    switch($var){
+        case "ok":
+            echo 'Status: ' . $value["status"] . "<br/> <br/>";
+            break;
+        case "error":
+            echo 'Status: ' . $value["status"] . "<br/>";
+            echo 'Message: ' . $value["message"] . "<br/>";
+            echo 'Raised: ' . $value["raised"] . "<br/>";
+            echo 'Updated: ' . $value["updated"] . "<br/>";
+            echo "<br/>";
+        }
 }
 //timestamp
 echo "<br/>";
